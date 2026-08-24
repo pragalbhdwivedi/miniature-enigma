98577e2c7e8ec91d46c7d2ec358c334f8a2ee9b5
Pragalbh Dwivedi <hitmanpragalbh@gmail.com>
2026-08-24 16:46:02 +0530

    Wire Module 11 itinerary navigator

--- src/main.jsx
+++ src/main.jsx
@@ -21,6 +21,7 @@ import './design-system/couple-editorial-hero.css'
 import './design-system/family-introduction.css'
 import './design-system/couple-story.css'
 import './design-system/corbett-destination.css'
+import './design-system/itinerary-navigator.css'
 import './cinematicEnhancements.js'
 import './sylvaKinetic.js'
 import './rsvpScrollGuard.js'
@@ -37,6 +38,7 @@ import './design-system/coupleEditorialHero.js'
 import './design-system/familyIntroduction.js'
 import './design-system/coupleStory.js'
 import './design-system/corbettDestination.js'
+import './design-system/itineraryNavigator.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
